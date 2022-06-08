b574aac46dfd4a20b64c80983971c5ed813d2913
anuvratsingh <anuvrats06@gmail.com>
2022-06-08 23:29:26 +0530

    Update: Parse TCP packets only

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use etherparse::Ipv4HeaderSlice;
+use etherparse::{Ipv4HeaderSlice, TcpHeaderSlice};
 use tun_tap::{Iface, Mode};
 
 fn main() {
@@ -8,18 +8,27 @@ fn main() {
         let n_bytes = nic.recv(&mut buf[..]).unwrap();
         let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
         let _eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
-       
+
         match Ipv4HeaderSlice::from_slice(&buf[4..n_bytes]) {
             Ok(p) => {
-                eprintln!(
-                    "Payload length: {} Protocol: {} Source: {} Destination:{}",
-                    p.payload_len(),
-                    p.protocol(),
-                    p.source_addr(),
-                    p.destination_addr()
-                );
+                let source = p.source_addr();
+                let destination = p.destination_addr();
+                let _protocol = p.protocol();
+
+                match TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(p) => {
+                        eprintln!(
+                            "{} -> {} {}b of tcp to port {}",
+                            source,
+                            destination,
+                            p.slice().len(),
+                            p.destination_port()
+                        );
+                    }
+                    Err(e) => eprintln!("Unknown TCP packet {}", e),
+                }
             }
-            Err(e) => eprintln!("Packet {} discarded", e),
+            Err(e) => eprintln!("Unknown packet {}", e),
         }
     }
 }
